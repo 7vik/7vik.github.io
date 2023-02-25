dd9cd0f59617f9fde879a8b6683ab849551afd92
Satvik Golechha <zsatvik@gmail.com>
2023-02-25 13:35:11 +0530

    add vinyasa

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,13 +39,3 @@ for (let i = 0; i < themeBtn.length; i++) {
   })
 
 }
-
-var clicks = 15;
-
-document.getElementById("clicks").innerHTML = clicks;
-
-$('.like-counter').click(function() {
-  clicks += 1;
-  document.getElementById("clicks").innerHTML = clicks;
-  $('.like-counter').addClass("liked");
-});
